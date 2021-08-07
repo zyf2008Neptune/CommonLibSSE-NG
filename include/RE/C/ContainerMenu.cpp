a69f66f23a4e12bfa772c45f8cf383db87c5ecb4
Parapets <southwest85b@gmail.com>
2021-08-07 19:09:55 +0100

    Offsets for ContainerMenu

--- include/RE/C/ContainerMenu.cpp
+++ include/RE/C/ContainerMenu.cpp
@@ -4,13 +4,21 @@ namespace RE
 {
 	ContainerMenu::ContainerMode ContainerMenu::GetContainerMode()
 	{
+#ifndef SKYRIMVR
 		REL::Relocation<ContainerMode*> mode{ REL::ID(519396) };
+#else
+		REL::Relocation<ContainerMode*> mode{ REL::Offset(0x3011720) };
+#endif
 		return *mode;
 	}
 
 	RefHandle ContainerMenu::GetTargetRefHandle()
 	{
+#ifndef SKYRIMVR
 		REL::Relocation<RefHandle*> handle{ REL::ID(519421) };
+#else
+		REL::Relocation<RefHandle*> handle{ REL::Offset(0x30117e8) };
+#endif
 		return *handle;
 	}
 }
