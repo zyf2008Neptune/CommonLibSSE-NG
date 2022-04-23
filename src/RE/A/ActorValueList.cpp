4803699e60508a8662abb0a43356d23e17476e66
Charmed Baryon <dev@charmedbaryon.com>
2022-04-23 16:21:16 -0700

    Fix `ActorValueList` warnings in Clang caused by unnecessary `nullptr` checks.

--- src/RE/A/ActorValueList.cpp
+++ src/RE/A/ActorValueList.cpp
@@ -6,7 +6,7 @@ namespace RE
 {
 	ActorValueInfo* ActorValueList::GetActorValue(ActorValue a_actorValue)
 	{
-		return actorValues && a_actorValue < ActorValue::kTotal ?
+		return a_actorValue < ActorValue::kTotal ?
                    actorValues[stl::to_underlying(a_actorValue)] :
                    nullptr;
 	}
@@ -15,12 +15,10 @@ namespace RE
 	{
 		ActorValue value = ActorValue::kNone;
 
-		if (actorValues) {
-			for (std::uint32_t i = 0; i < 164; i++) {
-				if (stl::string::iequals(actorValues[i]->enumName, a_enumName)) {
-					value = static_cast<ActorValue>(i);
-					break;
-				}
+		for (std::uint32_t i = 0; i < 164; i++) {
+			if (stl::string::iequals(actorValues[i]->enumName, a_enumName)) {
+				value = static_cast<ActorValue>(i);
+				break;
 			}
 		}
 
