1189c5cb802a44642df42e1140685dac30e59dda
Charmed Baryon <dev@charmedbaryon.com>
2022-04-23 16:25:01 -0700

    Fix warning caused by shadowed template variables.

--- include/RE/B/BSTAnimationGraphDataChannel.h
+++ include/RE/B/BSTAnimationGraphDataChannel.h
@@ -4,7 +4,7 @@
 
 namespace RE
 {
-	template <class T, class Val, template <class T, class Val> class Channel>
+	template <class T, class Val, template <class U, class UVal> class Channel>
 	class BSTAnimationGraphDataChannel :
 		public BSAnimationGraphChannel,  // 00
 		public Channel<T, Val>           // 20
