e9c7ab25834a33cc0c43c81665a679da5f6f0bb2
powerof3 <powerof3@users.noreply.github.com>
2022-04-26 04:34:27 +0000

    maintenance

--- include/RE/H/HorseCameraState.h
+++ include/RE/H/HorseCameraState.h
@@ -15,17 +15,17 @@ namespace RE
 		virtual ~HorseCameraState();  // 00
 
 		// override (ThirdPersonState)
-        void Begin() override;                                    // 01
-        void End() override;                                      // 02
-        void SaveGame(BGSSaveFormBuffer* a_buf) override;         // 06
-        void LoadGame(BGSLoadFormBuffer* a_buf) override;         // 07
-        void Revert(BGSLoadFormBuffer* a_buf) override;           // 08
-        void SetCameraHandle(RefHandle& a_handle) override;       // 09 - { return; }
-        void Unk_0A(void) override;                               // 0A - { return; }
-        void ProcessWeaponDrawnChange(bool a_drawn) override;     // 0B
-        bool GetFreeRotationMode() const override;                // 0C
-        void SetFreeRotationMode(bool a_weaponSheathed) override; // 0D
-        void HandleLookInput(const NiPoint2& a_input) override;   // 0F
+		void Begin() override;                                     // 01
+		void End() override;                                       // 02
+		void SaveGame(BGSSaveFormBuffer* a_buf) override;          // 06
+		void LoadGame(BGSLoadFormBuffer* a_buf) override;          // 07
+		void Revert(BGSLoadFormBuffer* a_buf) override;            // 08
+		void SetCameraHandle(RefHandle& a_handle) override;        // 09 - { return; }
+		void Unk_0A(void) override;                                // 0A - { return; }
+		void ProcessWeaponDrawnChange(bool a_drawn) override;      // 0B
+		bool GetFreeRotationMode() const override;                 // 0C
+		void SetFreeRotationMode(bool a_weaponSheathed) override;  // 0D
+		void HandleLookInput(const NiPoint2& a_input) override;    // 0F
 
 		// members
 		ObjectRefHandle horseRefHandle;         // E8
