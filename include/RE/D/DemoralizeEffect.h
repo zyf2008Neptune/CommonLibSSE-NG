2fc6a7a8c407fc1b59bb19b941b1d2e1646dd12e
powerof3 <powerof3@users.noreply.github.com>
2022-05-12 06:19:09 +0000

    maintenance

--- include/RE/D/DemoralizeEffect.h
+++ include/RE/D/DemoralizeEffect.h
@@ -13,7 +13,7 @@ namespace RE
 		// override (TargetValueModifierEffect)
 		~DemoralizeEffect() override;  // 13
 
-	    float GetTargetValue() const override;  // 21 - { return 0.0f; }
+		float GetTargetValue() const override;  // 21 - { return 0.0f; }
 	};
 	static_assert(sizeof(DemoralizeEffect) == 0x98);
 }
