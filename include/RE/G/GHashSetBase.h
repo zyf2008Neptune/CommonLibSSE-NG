ee1775eccf688b7272224a2804f0a881749eb3a6
Charmed Baryon <dev@charmedbaryon.live>
2022-05-27 13:22:19 -0700

    Make builds use strict standards compliance.

--- include/RE/G/GHashSetBase.h
+++ include/RE/G/GHashSetBase.h
@@ -172,7 +172,7 @@ namespace RE
 			SetCapacity(this, a_sizeHint);
 		}
 
-		explicit GHashSetBase(void* a_memAddr) :
+		explicit GHashSetBase([[maybe_unused]] void* a_memAddr) :
 			table(0)
 		{}
 
