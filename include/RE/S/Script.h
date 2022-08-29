eab91bad120ef101fda7e1956c293d6e93f88be8
shad0wshayd3 <shad0wshayd3@users.noreply.github.com>
2022-08-28 22:13:28 -0600

    add `Script::ParseParameters`

--- include/RE/S/Script.h
+++ include/RE/S/Script.h
@@ -47,6 +47,15 @@ namespace RE
 		[[nodiscard]] std::string GetCommand() const;
 		void                      SetCommand(std::string_view a_command);
 
+		template <class... Args>
+		static bool ParseParameters(const SCRIPT_PARAMETER* a_paramInfo, SCRIPT_FUNCTION::ScriptData* a_scriptData, std::uint32_t& a_opcodeOffsetPtr, TESObjectREFR* a_thisObj, TESObjectREFR* a_containingObj, Script* a_scriptObj, ScriptLocals* a_locals, Args... a_args)
+		{
+			static_assert((std::is_pointer_v<Args> && ...), "arguments must all be pointers");
+			using func_t = bool(const SCRIPT_PARAMETER*, SCRIPT_FUNCTION::ScriptData*, std::uint32_t&, TESObjectREFR*, TESObjectREFR*, Script*, ScriptLocals*, ...);
+			REL::Relocation<func_t> func{ RELOCATION_ID(21425, 21910) };
+			return func(a_paramInfo, a_scriptData, a_opcodeOffsetPtr, a_thisObj, a_containingObj, a_scriptObj, a_locals, a_args...);
+		}
+
 		// members
 		SCRIPT_HEADER                           header;                       // 20
 		std::uint32_t                           pad34;                        // 34
