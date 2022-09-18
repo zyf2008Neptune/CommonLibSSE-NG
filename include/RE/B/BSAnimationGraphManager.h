750d0d0b0db877a84b52084316bfa1df6f100e2f
Charmed Baryon <dev@charmedbaryon.live>
2022-09-18 08:33:49 -0700

    Fix `BSAnimationGraphManager` for post-629 struct layout.

--- include/RE/B/BSAnimationGraphManager.h
+++ include/RE/B/BSAnimationGraphManager.h
@@ -37,8 +37,15 @@ namespace RE
 		BSTArray<AnimVariableCacheInfo> variableCache;  // 00
 		void*                           unk18;          // 18
 		void*                           unk20;          // 20 - smart ptr
+#if !defined(ENABLE_SKYRIM_SE) && !defined(ENABLE_SKYRIM_VR)
+		void*                           unk28;          // 28
+#endif
 	};
+#if !defined(ENABLE_SKYRIM_SE) && !defined(ENABLE_SKYRIM_VR)
+	static_assert(sizeof(BSAnimationGraphVariableCache) == 0x30);
+#else
 	static_assert(sizeof(BSAnimationGraphVariableCache) == 0x28);
+#endif
 
 	BSSmartPointer(BSAnimationGraphManager);
 
@@ -71,17 +78,41 @@ namespace RE
 		// override (BSTEventSink<BSAnimationGraphEvent>)
 		BSEventNotifyControl ProcessEvent(const BSAnimationGraphEvent* a_event, BSTEventSource<BSAnimationGraphEvent>* a_eventSource) override;  // 01
 
+		struct RUNTIME_DATA
+		{
+#define RUNTIME_DATA_CONTENT                                                \
+	mutable BSSpinLock                   updateLock;           /* 98, A0 */ \
+	mutable BSSpinLock                   dependentManagerLock; /* A0 */     \
+	std::uint32_t                        activeGraph;          /* A8 */     \
+	std::uint32_t                        generateDepth;        /* A8 */
+
+			RUNTIME_DATA_CONTENT
+		};
+
+		[[nodiscard]] inline RUNTIME_DATA& GetRuntimeData() noexcept
+		{
+			return REL::RelocateMember<RUNTIME_DATA>(this, 0x58, 0x60);
+		}
+
+		[[nodiscard]] inline const RUNTIME_DATA& GetRuntimeData() const noexcept
+		{
+			return REL::RelocateMember<RUNTIME_DATA>(this, 0x58, 0x60);
+		}
+
 		// members
 		std::uint32_t                                       pad0C;                 // 0C
 		BSTArray<BSTSmartPointer<BSAnimationGraphChannel>>  boundChannels;         // 10
 		BSTArray<BSTSmartPointer<BSAnimationGraphChannel>>  bumpedChannels;        // 28
 		BSTSmallArray<BSTSmartPointer<BShkbAnimationGraph>> graphs;                // 40
-		BSTArray<BSAnimationGraphManagerPtr>                subManagers;           // 58
-		BSAnimationGraphVariableCache                       variableCache;         // 70
-		mutable BSSpinLock                                  updateLock;            // 98
-		mutable BSSpinLock                                  dependentManagerLock;  // A0
-		std::uint32_t                                       activeGraph;           // A8
-		std::uint32_t                                       generateDepth;         // A8
+		BSTArray<BSAnimationGraphManagerPtr> subManagers;                          // 58
+		BSAnimationGraphVariableCache        variableCache;                        // 70
+
+#ifndef ENABLE_SKYRIM_AE
+		RUNTIME_DATA_CONTENT
+#endif
 	};
+#ifndef ENABLE_SKYRIM_AE
 	static_assert(sizeof(BSAnimationGraphManager) == 0xB0);
+#endif
 }
+#undef RUNTIME_DATA_CONTENT
