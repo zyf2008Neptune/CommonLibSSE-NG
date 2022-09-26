0f6e5b7369a536282354b71d6de0a472516c029a
powerof3 <32599957+powerof3@users.noreply.github.com>
2022-09-27 01:57:20 +0530

    add missing `Projectile` vtables

--- include/RE/B/BeamProjectile.h
+++ include/RE/B/BeamProjectile.h
@@ -16,6 +16,7 @@ namespace RE
 	{
 	public:
 		inline static constexpr auto RTTI = RTTI_BeamProjectile;
+		inline static constexpr auto VTABLE = VTABLE_BeamProjectile;
 		inline static constexpr auto FORMTYPE = FormType::ProjectileBeam;
 
 		struct RecordFlags
