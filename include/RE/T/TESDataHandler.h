226bda3fcba19fa70d5a4ceb1f6b53c0e1f4c362
Alan Tse <alandtse@gmail.com>
2022-10-31 01:19:49 -0700

    Fix VR TESDataHandler

--- include/RE/T/TESDataHandler.h
+++ include/RE/T/TESDataHandler.h
@@ -79,25 +79,47 @@ namespace RE
 		std::uint32_t                     padD54;                                         // D54
 		TESFile*                          activeFile;                                     // D58
 		BSSimpleList<TESFile*>            files;                                          // D60
-		TESFileCollection                 compiledFileCollection;                         // D70 //may be broken in SKYRIMVR, iterate over files instead
-		bool                              masterSave;                                     // DA0
-		bool                              blockSave;                                      // DA1
-		bool                              saveLoadGame;                                   // DA2
-		bool                              autoSaving;                                     // DA3
-		bool                              exportingPlugin;                                // DA4
-		bool                              clearingData;                                   // DA5
-		bool                              hasDesiredFiles;                                // DA6
-		bool                              checkingModels;                                 // DA7
-		bool                              loadingFiles;                                   // DA8
-		bool                              dontRemoveIDs;                                  // DA9
-		std::uint8_t                      unkDAA;                                         // DAA
-		std::uint8_t                      padDAB;                                         // DAB
-		std::uint32_t                     padDAC;                                         // DAC
-		TESRegionDataManager*             regionDataManager;                              // DB0
-		InventoryChanges*                 merchantInventory;                              // DB8
+#ifndef SKYRIMVR
+		TESFileCollection     compiledFileCollection;  // D70 //may be broken in SKYRIMVR, iterate over files instead
+		bool                  masterSave;              // DA0
+		bool                  blockSave;               // DA1
+		bool                  saveLoadGame;            // DA2
+		bool                  autoSaving;              // DA3
+		bool                  exportingPlugin;         // DA4
+		bool                  clearingData;            // DA5
+		bool                  hasDesiredFiles;         // DA6
+		bool                  checkingModels;          // DA7
+		bool                  loadingFiles;            // DA8
+		bool                  dontRemoveIDs;           // DA9
+		std::uint8_t          unkDAA;                  // DAA
+		std::uint8_t          padDAB;                  // DAB
+		std::uint32_t         padDAC;                  // DAC
+		TESRegionDataManager* regionDataManager;       // DB0
+		InventoryChanges*     merchantInventory;       // DB8
+#else
+		std::uint32_t         loadedModCount;     // D70
+		std::uint32_t         pad14;              // D74
+		TESFile*              loadedMods[0xFF];   // D78
+		bool                  masterSave;         // 1570
+		bool                  blockSave;          // 1571
+		bool                  saveLoadGame;       // 1572
+		bool                  autoSaving;         // 1574
+		bool                  exportingPlugin;    // 1575
+		bool                  clearingData;       // 1576 - init'd to 1
+		bool                  hasDesiredFiles;    // 1577
+		bool                  checkingModels;     // 1578
+		bool                  loadingFiles;       // 1579
+		bool                  dontRemoveIDs;      // 157A
+		std::uint8_t          pad157B[5];         // 157B
+		TESRegionDataManager* regionDataManager;  // 1580
+		InventoryChanges*     merchantInventory;  // 1588
+#endif
 	};
+#ifndef SKYRIMVR
 	static_assert(sizeof(TESDataHandler) == 0xDC0);
-
+#else
+	static_assert(sizeof(TESDataHandler) == 0x1590);
+#endif
 	template <class T>
 	T* TESDataHandler::LookupForm(FormID a_rawFormID, std::string_view a_modName)
 	{
--- src/RE/T/TESDataHandler.cpp
+++ src/RE/T/TESDataHandler.cpp
@@ -68,7 +68,13 @@ namespace RE
 
 	const TESFile* TESDataHandler::LookupLoadedModByName(std::string_view a_modName)
 	{
-		for (auto& file : compiledFileCollection.files) {
+		for (auto& file : 
+#ifndef SKYRIMVR
+			compiledFileCollection.files
+#else
+			files
+#endif
+			) {
 			if (a_modName.size() == strlen(file->fileName) &&
 				_strnicmp(file->fileName, a_modName.data(), a_modName.size()) == 0) {
 				return file;
@@ -79,7 +85,13 @@ namespace RE
 
 	const TESFile* TESDataHandler::LookupLoadedModByIndex(std::uint8_t a_index)
 	{
-		for (auto& file : compiledFileCollection.files) {
+		for (auto& file :
+#ifndef SKYRIMVR
+			compiledFileCollection.files
+#else
+			files
+#endif
+		) {
 			if (file->compileIndex == a_index) {
 				return file;
 			}
@@ -93,24 +105,36 @@ namespace RE
 		return mod ? std::make_optional(mod->compileIndex) : std::nullopt;
 	}
 
-	const TESFile* TESDataHandler::LookupLoadedLightModByName(std::string_view a_modName)
+	const TESFile* TESDataHandler::LookupLoadedLightModByName(std::string_view 
+#ifndef SKYRIMVR
+		a_modName
+#endif
+	)
 	{
+#ifndef SKYRIMVR
 		for (auto& smallFile : compiledFileCollection.smallFiles) {
 			if (a_modName.size() == strlen(smallFile->fileName) &&
 				_strnicmp(smallFile->fileName, a_modName.data(), a_modName.size()) == 0) {
 				return smallFile;
 			}
 		}
+#endif
 		return nullptr;
 	}
 
-	const TESFile* TESDataHandler::LookupLoadedLightModByIndex(std::uint16_t a_index)
+	const TESFile* TESDataHandler::LookupLoadedLightModByIndex(std::uint16_t 
+#ifndef SKYRIMVR
+		a_index
+#endif
+	)
 	{
+#ifndef SKYRIMVR
 		for (auto& smallFile : compiledFileCollection.smallFiles) {
 			if (smallFile->smallFileCompileIndex == a_index) {
 				return smallFile;
 			}
 		}
+#endif
 		return nullptr;
 	}
 
