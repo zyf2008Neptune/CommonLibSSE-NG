1e7daf9c0f27a19d338afc8eb6445f78c9147cd6
powerof3 <32599957+powerof3@users.noreply.github.com>
2022-11-04 09:19:11 +0530

    Misc fixes

--- include/RE/H/Hazard.h
+++ include/RE/H/Hazard.h
@@ -42,9 +42,9 @@ namespace RE
 		bool OnAddCellPerformQueueReference(TESObjectCELL& a_cell) const override;  // 90 - { return false; }
 
 		// add
-		virtual void Initialize();                // A2
-		virtual void Unk_A3(void);                // A3 - { return; }
-		virtual bool IsNotGeneratedForm() const;  // A4 - { return TESDataHandler::GetSingleton()->IsGeneratedFormID(formID) == 0; }
+		virtual void Initialize();         // A2
+		virtual void Unk_A3(void);         // A3 - { return; }
+		virtual bool IsPermanent() const;  // A4 - { return TESDataHandler::GetSingleton()->IsGeneratedFormID(formID) == 0; }
 
 		// members
 		void*                                  hazardDBHandle;  // 98
