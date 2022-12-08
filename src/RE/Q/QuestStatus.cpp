78a5b12e5c0b52d5de9996b0bdf3511bdb94496c
powerof3 <32599957+powerof3@users.noreply.github.com>
2022-12-08 21:45:58 +0530

    Fix compile errors

--- src/RE/Q/QuestStatus.cpp
+++ src/RE/Q/QuestStatus.cpp
@@ -1,13 +1,11 @@
 #include "RE/Q/QuestStatus.h"
 
-#include "Offsets.h"
-
 namespace RE
 {
 	BSTEventSource<QuestStatus::Event>* QuestStatus::GetEventSource()
 	{
 		using func_t = decltype(&QuestStatus::GetEventSource);
-		REL::Relocation<func_t> func{ 24719, 25196 };
+		REL::Relocation<func_t> func{ RELOCATION_ID(24719, 25196) };
 		return func();
 	}
 }
