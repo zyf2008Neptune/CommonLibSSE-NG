c3277ac131e976a41669b7740cbac99db5e678dc
powerof3 <32599957+powerof3@users.noreply.github.com>
2023-01-01 17:10:38 +0530

    Fix `FastTravelConfirmCallback`, `MenuModeChangeEvent`

--- include/RE/M/MenuModeChangeEvent.h
+++ include/RE/M/MenuModeChangeEvent.h
@@ -4,15 +4,16 @@
 
 namespace RE
 {
-	struct MenuModeChangeEvent
+	class MenuModeChangeEvent
 	{
-		enum Mode : std::uint8_t
+		enum class Mode
 		{
+			kNone = static_cast<std::underlying_type_t<Mode>>(-1),
 			kHidden = 0,
 			kDisplayed = 1
 		};
 
-		BSFixedString menu;  // 00
-		Mode          mode;  // 08
+		BSFixedString                        menu;  // 00
+		stl::enumeration<Mode, std::uint8_t> mode;  // 08
 	};
 }
