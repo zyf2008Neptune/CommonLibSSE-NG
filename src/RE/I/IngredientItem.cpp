022a7cffac437752c85b930fc3c4cee6dc1cc531
powerof3 <32599957+powerof3@users.noreply.github.com>
2023-01-21 12:09:56 +0530

    Update `IngredientItem`

--- src/RE/I/IngredientItem.cpp
+++ src/RE/I/IngredientItem.cpp
@@ -4,58 +4,54 @@
 
 namespace RE
 {
-	// 14097CD70 (1.5.97)
 	bool IngredientItem::IsHostile() const
 	{
-		return this->hostileCount > 0;
+		return hostileCount > 0;
 	}
 
-	// 1400FF120 (1.5.97)
-	bool IngredientItem::LearnEffect(std::uint32_t a_aiIndex)
+	bool IngredientItem::LearnEffect(std::uint32_t a_index)
 	{
-		if (a_aiIndex < 4) {
-			this->gamedata.knownEffectFlags |= 1 << a_aiIndex;
-			this->AddChange(0x80000000);
+		if (a_index < 4) {
+			gamedata.knownEffectFlags |= 1 << a_index;
+			AddChange(ChangeFlags::kIngredientUse);
 			return true;
 		}
 		return false;
 	}
 
-	bool IngredientItem::LearnEffect(RE::EffectSetting* a_effect)
+	bool IngredientItem::LearnEffect(EffectSetting* a_effect)
 	{
 		if (!a_effect) {
 			return false;
 		}
-		std::uint32_t aiIndex = 0;
-		for (auto effect : this->effects) {
+		std::uint32_t index = 0;
+		for (auto& effect : effects) {
 			if (effect->baseEffect == a_effect) {
-				LearnEffect(aiIndex);
+				LearnEffect(index);
 				return true;
 			}
-			aiIndex++;
+			index++;
 		}
 		return false;
 	}
 
-	// 1400FF1A0 (1.5.97)
-	int IngredientItem::LearnNextEffect()
+	std::optional<std::uint32_t> IngredientItem::LearnNextEffect()
 	{
-		long flags = this->gamedata.knownEffectFlags;
-		auto aiIndex = 0;
-		while ((flags & 1 << aiIndex) != 0) {
-			if (++aiIndex >= 4) {
-				return -1;
+		std::uint16_t flags = gamedata.knownEffectFlags;
+		std::uint32_t index = 0;
+		while ((flags & 1 << index) != 0) {
+			if (++index >= 4) {
+				return std::nullopt;
 			}
 		}
-		LearnEffect(aiIndex);
-		return aiIndex;
+		LearnEffect(index);
+		return index;
 	}
 
-	// 1400FF150 (1.5.97)
 	void IngredientItem::LearnAllEffects()
 	{
-		for (std::uint32_t aiIndex = 0; aiIndex < 4; ++aiIndex) {
-			LearnEffect(aiIndex);
+		for (std::uint32_t index = 0; index < 4; ++index) {
+			LearnEffect(index);
 		}
 	}
 }
