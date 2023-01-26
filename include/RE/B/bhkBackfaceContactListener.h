f9078d342ea21f6a8301bdb9a26a7c65de70c132
powerof3 <powerof3@users.noreply.github.com>
2023-01-26 02:56:20 +0000

    maintenance

--- include/RE/B/bhkBackfaceContactListener.h
+++ include/RE/B/bhkBackfaceContactListener.h
@@ -9,7 +9,7 @@ namespace RE
 	public:
 		inline static constexpr auto RTTI = RTTI_bhkBackfaceContactListener;
 
-        ~bhkBackfaceContactListener() override;  // 04
+		~bhkBackfaceContactListener() override;  // 04
 	};
 	static_assert(sizeof(bhkBackfaceContactListener) == 0x8);
 }
