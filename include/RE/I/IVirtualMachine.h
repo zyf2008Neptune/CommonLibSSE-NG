bb030f967df843d741a4e09fd6fa64e4cc12bc48
Nightfallstorm <Nightfallstorm@users.noreply.github.com>
2023-02-07 06:48:33 +0000

    maintenance

--- include/RE/I/IVirtualMachine.h
+++ include/RE/I/IVirtualMachine.h
@@ -122,9 +122,8 @@ namespace RE
 			void RegisterLatentFunction(std::string_view a_fnName, std::string_view a_className, F a_callback, bool a_callableFromTasklets = false);
 
 			template <class V>
-				requires is_return_convertible_v<V>
+			requires is_return_convertible_v<V>
 			void ReturnLatentResult(VMStackID a_stackID, V result);
-
 
 			void SetCallableFromTasklets(const char* a_className, const char* a_stateName, const char* a_fnName, bool a_callable);
 			void SetCallableFromTasklets(const char* a_className, const char* a_fnName, bool a_callable);
