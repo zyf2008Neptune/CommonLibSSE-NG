75d549e7ee15d718232718604aa8afe6e82bb405
Nightfallstorm <Nightfallstorm@users.noreply.github.com>
2023-02-12 00:12:31 +0000

    maintenance

--- include/RE/I/INIPrefSettingCollection.h
+++ include/RE/I/INIPrefSettingCollection.h
@@ -13,9 +13,9 @@ namespace RE
 		~INIPrefSettingCollection() override;  // 00
 
 		// override (INISettingCollection)
-		void Unk_07(void) override;  // 07 - { return 1; }
+		void Unk_07(void) override;        // 07 - { return 1; }
 		void WriteAllSettings() override;  // 08
-		void ReadAllSettings() override;  // 09
+		void ReadAllSettings() override;   // 09
 
 		static INIPrefSettingCollection* GetSingleton();
 	};
