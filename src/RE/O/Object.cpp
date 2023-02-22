1923db6ad0d0ea71870e56ac8977e9a5e66b3d70
Parapets <southwest85b@gmail.com>
2023-02-22 12:54:22 +0000

    Add BSScript::Object::GetVariable

--- src/RE/O/Object.cpp
+++ src/RE/O/Object.cpp
@@ -75,6 +75,42 @@ namespace RE
 			return idx != INVALID ? std::addressof(variables[idx]) : nullptr;
 		}
 
+		Variable* Object::GetVariable(const BSFixedString& a_name)
+		{
+			return const_cast<Variable*>(
+				const_cast<const Object*>(this)->GetVariable(a_name));
+		}
+
+		const Variable* Object::GetVariable(const BSFixedString& a_name) const
+		{
+			constexpr auto INVALID = static_cast<std::uint32_t>(-1);
+
+			auto          idx = INVALID;
+			std::uint32_t offset = 0;
+			for (auto cls = type.get(); cls; cls = cls->GetParent()) {
+				const auto vars = cls->GetVariableIter();
+				if (idx == INVALID) {
+					if (vars) {
+						for (std::uint32_t i = 0; i < cls->GetNumVariables(); i++) {
+							const auto& var = vars[i];
+							if (var.name == a_name) {
+								idx = i;
+								break;
+							}
+						}
+					}
+				} else {
+					offset += cls->GetNumVariables();
+				}
+			}
+
+			if (idx == INVALID) {
+				return nullptr;
+			}
+
+			return std::addressof(variables[offset + idx]);
+		}
+
 		void Object::Dtor()
 		{
 			using func_t = decltype(&Object::Dtor);
