a9bff872fc079165a9a18db19dc12e9ed2bb660a
Qudix <17361645+Qudix@users.noreply.github.com>
2023-03-31 03:25:16 -0500

    Reluctantly remove `Actor::VTABLE`

--- include/RE/A/Actor.h
+++ include/RE/A/Actor.h
@@ -130,7 +130,6 @@ namespace RE
 
 	public:
 		inline static constexpr auto RTTI = RTTI_Actor;
-		inline static constexpr auto VTABLE = VTABLE_Actor;
 		inline static constexpr auto FORMTYPE = FormType::ActorCharacter;
 
 		struct SlotTypes
