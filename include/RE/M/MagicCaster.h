4ee96f439bf913578d98507ff7188bfce72d0364
shad0wshayd3 <shad0wshayd3@users.noreply.github.com>
2023-04-06 10:27:48 -0600

    move MagicCaster functions

--- include/RE/M/MagicCaster.h
+++ include/RE/M/MagicCaster.h
@@ -72,56 +72,14 @@ namespace RE
 		virtual void                       PrepareSound(MagicSystem::SoundID a_sound, MagicItem* a_spell);                                                                                                                                  // 1B
 		virtual void                       AdjustActiveEffect(ActiveEffect* a_activeEffect, float a_power, bool a_arg3);                                                                                                                    // 1C
 
-		MagicTarget* FindPickTarget(NiPoint3& a_targetLocation, TESObjectCELL** a_targetCell, bhkPickData& a_pickData)
-		{
-			using func_t = decltype(&MagicCaster::FindPickTarget);
-			REL::Relocation<func_t> func{ RELOCATION_ID(33676, 34456) };
-			return func(this, a_targetLocation, a_targetCell, a_pickData);
-		}
-
-		bool FindTargets(float a_effectivenessMult, std::uint32_t& a_targetCount, TESBoundObject* a_source, bool a_loadCast, bool a_adjustOnlyHostileEffectiveness)
-		{
-			using func_t = decltype(&MagicCaster::FindTargets);
-			REL::Relocation<func_t> func{ RELOCATION_ID(33676, 34456) };
-			return func(this, a_effectivenessMult, a_targetCount, a_source, a_loadCast, a_adjustOnlyHostileEffectiveness);
-		}
-
-		void FinishCast()
-		{
-			using func_t = decltype(&MagicCaster::FinishCast);
-			REL::Relocation<func_t> func{ RELOCATION_ID(33657, 34435) };
-			return func(this);
-		}
-
-		float GetCurrentSpellCost()
-		{
-			using func_t = decltype(&MagicCaster::GetCurrentSpellCost);
-			REL::Relocation<func_t> func{ RELOCATION_ID(33426, 34204) };
-			return func(this);
-		}
-
-		void InterruptCast(bool a_refund)
-		{
-			using func_t = decltype(&MagicCaster::InterruptCast);
-			REL::Relocation<func_t> func{ RELOCATION_ID(33630, 34408) };
-			return func(this, a_refund);
-		}
-
-		void PlayReleaseSound(MagicItem* a_item)
-		{
-			using func_t = decltype(&MagicCaster::PlayReleaseSound);
-			REL::Relocation<func_t> func{ RELOCATION_ID(33675, 34448) };
-			return func(this, a_item);
-		}
-
-		void UpdateImpl(float a_delta)
-		{
-			using func_t = decltype(&MagicCaster::UpdateImpl);
-			REL::Relocation<func_t> func{ RELOCATION_ID(33622, 34400) };
-			return func(this, a_delta);
-		}
-
-		bool TestProjectilePlacement(const Effect& a_effect, const bhkPickData& a_pickData);
+		MagicTarget* FindPickTarget(NiPoint3& a_targetLocation, TESObjectCELL** a_targetCell, bhkPickData& a_pickData);
+		bool         FindTargets(float a_effectivenessMult, std::uint32_t& a_targetCount, TESBoundObject* a_source, bool a_loadCast, bool a_adjustOnlyHostileEffectiveness);
+		void         FinishCast();
+		float        GetCurrentSpellCost();
+		void         InterruptCast(bool a_refund);
+		void         PlayReleaseSound(MagicItem* a_item);
+		bool         TestProjectilePlacement(const Effect& a_effect, const bhkPickData& a_pickData);
+		void         UpdateImpl(float a_delta);
 
 		// members
 		BSTArray<BSSoundHandle>                sounds;             // 08
--- src/RE/M/MagicCaster.cpp
+++ src/RE/M/MagicCaster.cpp
@@ -6,6 +6,48 @@
 
 namespace RE
 {
+	MagicTarget* MagicCaster::FindPickTarget(NiPoint3& a_targetLocation, TESObjectCELL** a_targetCell, bhkPickData& a_pickData)
+	{
+		using func_t = decltype(&MagicCaster::FindPickTarget);
+		REL::Relocation<func_t> func{ RELOCATION_ID(33676, 34456) };
+		return func(this, a_targetLocation, a_targetCell, a_pickData);
+	}
+
+	bool MagicCaster::FindTargets(float a_effectivenessMult, std::uint32_t& a_targetCount, TESBoundObject* a_source, bool a_loadCast, bool a_adjustOnlyHostileEffectiveness)
+	{
+		using func_t = decltype(&MagicCaster::FindTargets);
+		REL::Relocation<func_t> func{ RELOCATION_ID(33676, 34456) };
+		return func(this, a_effectivenessMult, a_targetCount, a_source, a_loadCast, a_adjustOnlyHostileEffectiveness);
+	}
+
+	void MagicCaster::FinishCast()
+	{
+		using func_t = decltype(&MagicCaster::FinishCast);
+		REL::Relocation<func_t> func{ RELOCATION_ID(33657, 34435) };
+		return func(this);
+	}
+
+	float MagicCaster::GetCurrentSpellCost()
+	{
+		using func_t = decltype(&MagicCaster::GetCurrentSpellCost);
+		REL::Relocation<func_t> func{ RELOCATION_ID(33426, 34204) };
+		return func(this);
+	}
+
+	void MagicCaster::InterruptCast(bool a_refund)
+	{
+		using func_t = decltype(&MagicCaster::InterruptCast);
+		REL::Relocation<func_t> func{ RELOCATION_ID(33630, 34408) };
+		return func(this, a_refund);
+	}
+
+	void MagicCaster::PlayReleaseSound(MagicItem* a_item)
+	{
+		using func_t = decltype(&MagicCaster::PlayReleaseSound);
+		REL::Relocation<func_t> func{ RELOCATION_ID(33675, 34448) };
+		return func(this, a_item);
+	}
+
 	bool MagicCaster::TestProjectilePlacement(const Effect& a_effect, const bhkPickData& a_pickData)
 	{
 		if (auto baseEffect = a_effect.baseEffect) {
@@ -30,5 +72,12 @@ namespace RE
 		}
 
 		return true;
+	}
+
+	void MagicCaster::UpdateImpl(float a_delta)
+	{
+		using func_t = decltype(&MagicCaster::UpdateImpl);
+		REL::Relocation<func_t> func{ RELOCATION_ID(33622, 34400) };
+		return func(this, a_delta);
 	}
 }
