bc3962e41682e48e0eadbd6b5b1be882d19d1d46
shad0wshayd3 <shad0wshayd3@users.noreply.github.com>
2023-04-06 09:33:54 -0600

    add SendSpellCastEvent

--- include/RE/S/ScriptEventSourceHolder.h
+++ include/RE/S/ScriptEventSourceHolder.h
@@ -122,6 +122,7 @@ namespace RE
 
 		void SendActivateEvent(const NiPointer<TESObjectREFR>& a_objectActivated, const NiPointer<TESObjectREFR>& a_actionRef);
 		void SendOpenCloseEvent(const NiPointer<TESObjectREFR>& a_ref, const NiPointer<TESObjectREFR>& a_activeRef, bool a_isOpened);
+		void SendSpellCastEvent(const NiPointer<TESObjectREFR>& a_object, FormID a_formID);
 
 		template <class T>
 		inline BSTEventSource<T>* GetEventSource()
