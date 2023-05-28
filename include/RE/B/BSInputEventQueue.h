Exit-9B/CommonLibSSE

BSInputEventQueue adjustments for VR

include/RE/B/BSInputEventQueue.h
@@ -7,6 +7,8 @@
 #include "RE/K/KinectEvent.h"
 #include "RE/M/MouseMoveEvent.h"
 #include "RE/T/ThumbstickEvent.h"
+#include "RE/V/VrWandTouchpadPositionEvent.h"
+#include "RE/V/VrWandTouchpadSwipeEvent.h"
 
 namespace RE
 {
@@ -19,6 +21,10 @@ namespace RE
 		inline static constexpr std::uint8_t MAX_THUMBSTICK_EVENTS = 2;
 		inline static constexpr std::uint8_t MAX_CONNECT_EVENTS = 1;
 		inline static constexpr std::uint8_t MAX_KINECT_EVENTS = 1;
+#ifdef SKYRIMVR
+		inline static constexpr std::uint8_t MAX_VR_TOUCHPAD_POSITION_EVENTS = 3;
+		inline static constexpr std::uint8_t MAX_VR_TOUCHPAD_SWIPE_EVENTS = 3;
+#endif
 
 		static BSInputEventQueue* GetSingleton();
 
@@ -28,27 +34,45 @@ namespace RE
 		void AddThumbstickEvent(ThumbstickEvent::InputType a_id, float a_xValue, float a_yValue);
 		void AddConnectEvent(INPUT_DEVICE a_device, bool a_connected);
 		void AddKinectEvent(const BSFixedString& a_userEvent, const BSFixedString& a_heard);
+#ifdef SKYRIMVR
+		void AddButtonEvent(INPUT_DEVICE a_device, std::int32_t a_arg2, std::int32_t a_id, float a_value, float a_duration);
+		void AddThumbstickEvent(ThumbstickEvent::InputType a_id, INPUT_DEVICE a_device, float a_xValue, float a_yValue);
+#endif
 		void PushOntoInputQueue(InputEvent* a_event);
 		void ClearInputQueue();
 
 		// members
-		std::uint8_t       pad001;                                   // 001
-		std::uint16_t      pad002;                                   // 002
-		std::uint32_t      buttonEventCount;                         // 004
-		std::uint32_t      charEventCount;                           // 008
-		std::uint32_t      mouseEventCount;                          // 00C
-		std::uint32_t      thumbstickEventCount;                     // 010
-		std::uint32_t      connectEventCount;                        // 014
-		std::uint32_t      kinectEventCount;                         // 018
-		std::uint32_t      pad01C;                                   // 01C
+		std::uint8_t  pad001;                // 001
+		std::uint16_t pad002;                // 002
+		std::uint32_t buttonEventCount;      // 004
+		std::uint32_t charEventCount;        // 008
+		std::uint32_t mouseEventCount;       // 00C
+		std::uint32_t thumbstickEventCount;  // 010
+		std::uint32_t connectEventCount;     // 014
+		std::uint32_t kinectEventCount;      // 018
+#ifndef SKYRIMVR
+		std::uint32_t pad01C;  // 01C
+#else
+		std::uint32_t unkVR01C;
+		std::uint32_t vrTouchpadPositionEventCount;
+		std::uint32_t vrTouchpadSwipeEventCount;
+#endif
 		ButtonEvent        buttonEvents[MAX_BUTTON_EVENTS];          // 020
 		CharEvent          charEvents[MAX_CHAR_EVENTS];              // 200
 		MouseMoveEvent     mouseEvents[MAX_MOUSE_EVENTS];            // 2A0
 		ThumbstickEvent    thumbstickEvents[MAX_THUMBSTICK_EVENTS];  // 2D0
 		DeviceConnectEvent connectEvents[MAX_CONNECT_EVENTS];        // 330
 		KinectEvent        kinectEvents[MAX_KINECT_EVENTS];          // 350
-		InputEvent*        queueHead;                                // 380
-		InputEvent*        queueTail;                                // 388
+#ifdef SKYRIMVR
+		VrWandTouchpadPositionEvent vrTouchpadPositionEvents[MAX_VR_TOUCHPAD_POSITION_EVENTS];
+		VrWandTouchpadSwipeEvent    vrTouchpadSwipeEvents[MAX_VR_TOUCHPAD_SWIPE_EVENTS];
+#endif
+		InputEvent* queueHead;  // 380
+		InputEvent* queueTail;  // 388
 	};
+#ifndef SKYRIMVR
 	static_assert(sizeof(BSInputEventQueue) == 0x390);
+#else
+	static_assert(sizeof(BSInputEventQueue) == 0x580);
+#endif
 }

include/RE/B/ButtonEvent.h
@@ -3,10 +3,15 @@
 #include "RE/B/BSFixedString.h"
 #include "RE/I/IDEvent.h"
 #include "RE/I/InputEvent.h"
+#include "RE/V/VRWandEvent.h"
 
 namespace RE
 {
+#ifndef SKYRIMVR
 	class ButtonEvent : public IDEvent
+#else
+	class ButtonEvent : public VRWandEvent
+#endif
 	{
 	public:
 		inline static constexpr auto RTTI = RTTI_ButtonEvent;
@@ -25,5 +30,9 @@ namespace RE
 		float value;         // 28
 		float heldDownSecs;  // 2C
 	};
+#ifndef SKYRIMVR
 	static_assert(sizeof(ButtonEvent) == 0x30);
+#else
+	static_assert(sizeof(ButtonEvent) == 0x38);
+#endif
 }

src/RE/B/BSInputEventQueue.cpp
@@ -17,6 +17,9 @@ namespace RE
 			cachedEvent.device = a_device;
 			cachedEvent.idCode = a_id;
 			cachedEvent.userEvent = {};
+#ifdef SKYRIMVR
+			cachedEvent.unkVR28 = -1;
+#endif
 
 			PushOntoInputQueue(&cachedEvent);
 			++buttonEventCount;
@@ -51,9 +54,10 @@ namespace RE
 	{
 		if (thumbstickEventCount < MAX_THUMBSTICK_EVENTS) {
 			auto& cachedEvent = thumbstickEvents[thumbstickEventCount];
-			cachedEvent.idCode = a_id;
 			cachedEvent.xValue = a_xValue;
 			cachedEvent.yValue = a_yValue;
+			cachedEvent.device = INPUT_DEVICE::kGamepad;
+			cachedEvent.idCode = a_id;
 			cachedEvent.userEvent = {};
 
 			PushOntoInputQueue(&cachedEvent);
@@ -84,6 +88,39 @@ namespace RE
 			++kinectEventCount;
 		}
 	}
+
+#ifdef SKYRIMVR
+	void BSInputEventQueue::AddButtonEvent(INPUT_DEVICE a_device, std::int32_t a_arg2, std::int32_t a_id, float a_value, float a_duration)
+	{
+		if (buttonEventCount < MAX_BUTTON_EVENTS) {
+			auto& cachedEvent = buttonEvents[buttonEventCount];
+			cachedEvent.value = a_value;
+			cachedEvent.heldDownSecs = a_duration;
+			cachedEvent.device = a_device;
+			cachedEvent.idCode = a_id;
+			cachedEvent.userEvent = {};
+			cachedEvent.unkVR28 = a_arg2;
+
+			PushOntoInputQueue(&cachedEvent);
+			++buttonEventCount;
+		}
+	}
+
+	void BSInputEventQueue::AddThumbstickEvent(ThumbstickEvent::InputType a_id, INPUT_DEVICE a_device, float a_xValue, float a_yValue)
+	{
+		if (thumbstickEventCount < MAX_THUMBSTICK_EVENTS) {
+			auto& cachedEvent = thumbstickEvents[thumbstickEventCount];
+			cachedEvent.xValue = a_xValue;
+			cachedEvent.yValue = a_yValue;
+			cachedEvent.device = a_device;
+			cachedEvent.idCode = a_id;
+			cachedEvent.userEvent = {};
+
+			PushOntoInputQueue(&cachedEvent);
+			++thumbstickEventCount;
+		}
+	}
+#endif
 
 	void BSInputEventQueue::PushOntoInputQueue(InputEvent* a_event)
 	{

zyf2008Neptune/CommonLibSSE-NG

include/RE/B/BSInputEventQueue.h
@@ -7,6 +7,8 @@
 #include "RE/K/KinectEvent.h"
 #include "RE/M/MouseMoveEvent.h"
 #include "RE/T/ThumbstickEvent.h"
+#include "RE/V/VrWandTouchpadPositionEvent.h"
+#include "RE/V/VrWandTouchpadSwipeEvent.h"
 
 namespace RE
 {
@@ -19,6 +21,10 @@ namespace RE
 		inline static constexpr std::uint8_t MAX_THUMBSTICK_EVENTS = 2;
 		inline static constexpr std::uint8_t MAX_CONNECT_EVENTS = 1;
 		inline static constexpr std::uint8_t MAX_KINECT_EVENTS = 1;
+#ifdef ENABLE_SKYRIM_VR
+		inline static constexpr std::uint8_t MAX_VR_TOUCHPAD_POSITION_EVENTS = 3;
+		inline static constexpr std::uint8_t MAX_VR_TOUCHPAD_SWIPE_EVENTS = 3;
+#endif
 
 		static BSInputEventQueue* GetSingleton();
 
@@ -28,27 +34,45 @@ namespace RE
 		void AddThumbstickEvent(ThumbstickEvent::InputType a_id, float a_xValue, float a_yValue);
 		void AddConnectEvent(INPUT_DEVICE a_device, bool a_connected);
 		void AddKinectEvent(const BSFixedString& a_userEvent, const BSFixedString& a_heard);
+#ifdef ENABLE_SKYRIM_VR
+		void AddButtonEvent(INPUT_DEVICE a_device, std::int32_t a_arg2, std::int32_t a_id, float a_value, float a_duration);
+		void AddThumbstickEvent(ThumbstickEvent::InputType a_id, INPUT_DEVICE a_device, float a_xValue, float a_yValue);
+#endif
 		void PushOntoInputQueue(InputEvent* a_event);
 		void ClearInputQueue();
 
 		// members
-		std::uint8_t       pad001;                                   // 001
-		std::uint16_t      pad002;                                   // 002
-		std::uint32_t      buttonEventCount;                         // 004
-		std::uint32_t      charEventCount;                           // 008
-		std::uint32_t      mouseEventCount;                          // 00C
-		std::uint32_t      thumbstickEventCount;                     // 010
-		std::uint32_t      connectEventCount;                        // 014
-		std::uint32_t      kinectEventCount;                         // 018
-		std::uint32_t      pad01C;                                   // 01C
+		std::uint8_t  pad001;                // 001
+		std::uint16_t pad002;                // 002
+		std::uint32_t buttonEventCount;      // 004
+		std::uint32_t charEventCount;        // 008
+		std::uint32_t mouseEventCount;       // 00C
+		std::uint32_t thumbstickEventCount;  // 010
+		std::uint32_t connectEventCount;     // 014
+		std::uint32_t kinectEventCount;      // 018
+#ifndef ENABLE_SKYRIM_VR
+		std::uint32_t pad01C;  // 01C
+#else
+		std::uint32_t unkVR01C;
+		std::uint32_t vrTouchpadPositionEventCount;
+		std::uint32_t vrTouchpadSwipeEventCount;
+#endif
 		ButtonEvent        buttonEvents[MAX_BUTTON_EVENTS];          // 020
 		CharEvent          charEvents[MAX_CHAR_EVENTS];              // 200
 		MouseMoveEvent     mouseEvents[MAX_MOUSE_EVENTS];            // 2A0
 		ThumbstickEvent    thumbstickEvents[MAX_THUMBSTICK_EVENTS];  // 2D0
 		DeviceConnectEvent connectEvents[MAX_CONNECT_EVENTS];        // 330
 		KinectEvent        kinectEvents[MAX_KINECT_EVENTS];          // 350
-		InputEvent*        queueHead;                                // 380
-		InputEvent*        queueTail;                                // 388
+#ifdef ENABLE_SKYRIM_VR
+		VrWandTouchpadPositionEvent vrTouchpadPositionEvents[MAX_VR_TOUCHPAD_POSITION_EVENTS];
+		VrWandTouchpadSwipeEvent    vrTouchpadSwipeEvents[MAX_VR_TOUCHPAD_SWIPE_EVENTS];
+#endif
+		InputEvent* queueHead;  // 380
+		InputEvent* queueTail;  // 388
 	};
+#ifndef ENABLE_SKYRIM_VR
 	static_assert(sizeof(BSInputEventQueue) == 0x390);
+#else
+	static_assert(sizeof(BSInputEventQueue) == 0x580);
+#endif
 }

include/RE/B/ButtonEvent.h
@@ -4,10 +4,15 @@
 #include "RE/I/IDEvent.h"
 #include "RE/I/InputEvent.h"
 #include "RE/M/MemoryManager.h"
+#include "RE/V/VRWandEvent.h"
 
 namespace RE
 {
+#ifndef ENABLE_SKYRIM_VR
 	class ButtonEvent : public IDEvent
+#else
+	class ButtonEvent : public VRWandEvent
+#endif
 	{
 	public:
 		inline static constexpr auto RTTI = RTTI_ButtonEvent;
@@ -44,5 +49,9 @@ namespace RE
 			return buttonEvent;
 		}
 	};
+#ifndef ENABLE_SKYRIM_VR
 	static_assert(sizeof(ButtonEvent) == 0x30);
+#else
+	static_assert(sizeof(ButtonEvent) == 0x38);
+#endif
 }

src/RE/B/BSInputEventQueue.cpp
@@ -17,6 +17,9 @@ namespace RE
 			cachedEvent.device = a_device;
 			cachedEvent.idCode = a_id;
 			cachedEvent.userEvent = {};
+#ifdef ENABLE_SKYRIM_VR
+			cachedEvent.unkVR28 = -1;
+#endif
 
 			PushOntoInputQueue(&cachedEvent);
 			++buttonEventCount;
@@ -51,9 +54,10 @@ namespace RE
 	{
 		if (thumbstickEventCount < MAX_THUMBSTICK_EVENTS) {
 			auto& cachedEvent = thumbstickEvents[thumbstickEventCount];
-			cachedEvent.idCode = a_id;
 			cachedEvent.xValue = a_xValue;
 			cachedEvent.yValue = a_yValue;
+			cachedEvent.device = INPUT_DEVICE::kGamepad;
+			cachedEvent.idCode = a_id;
 			cachedEvent.userEvent = {};
 
 			PushOntoInputQueue(&cachedEvent);
@@ -84,6 +88,39 @@ namespace RE
 			++kinectEventCount;
 		}
 	}
+
+#ifdef ENABLE_SKYRIM_VR
+	void BSInputEventQueue::AddButtonEvent(INPUT_DEVICE a_device, std::int32_t a_arg2, std::int32_t a_id, float a_value, float a_duration)
+	{
+		if (buttonEventCount < MAX_BUTTON_EVENTS) {
+			auto& cachedEvent = buttonEvents[buttonEventCount];
+			cachedEvent.value = a_value;
+			cachedEvent.heldDownSecs = a_duration;
+			cachedEvent.device = a_device;
+			cachedEvent.idCode = a_id;
+			cachedEvent.userEvent = {};
+			cachedEvent.unkVR28 = a_arg2;
+
+			PushOntoInputQueue(&cachedEvent);
+			++buttonEventCount;
+		}
+	}
+
+	void BSInputEventQueue::AddThumbstickEvent(ThumbstickEvent::InputType a_id, INPUT_DEVICE a_device, float a_xValue, float a_yValue)
+	{
+		if (thumbstickEventCount < MAX_THUMBSTICK_EVENTS) {
+			auto& cachedEvent = thumbstickEvents[thumbstickEventCount];
+			cachedEvent.xValue = a_xValue;
+			cachedEvent.yValue = a_yValue;
+			cachedEvent.device = a_device;
+			cachedEvent.idCode = a_id;
+			cachedEvent.userEvent = {};
+
+			PushOntoInputQueue(&cachedEvent);
+			++thumbstickEventCount;
+		}
+	}
+#endif
 
 	void BSInputEventQueue::PushOntoInputQueue(InputEvent* a_event)
 	{