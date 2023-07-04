f52f01a4b87a79f0aff09d987c912ac7737845c1
Alan Tse <alandtse@gmail.com>
2023-07-04 00:54:27 -0700

    Fix VR Playercamera

--- include/RE/P/PlayerCamera.h
+++ include/RE/P/PlayerCamera.h
@@ -32,7 +32,9 @@ namespace RE
 			kMount,
 			kBleedout,
 			kDragon,
-
+#ifdef SKYRIMVR
+			kVR,  // ".?AVVrCameraState@@"? by memory address it's after bleedout and before tween
+#endif
 			kTotal
 		};
 	};
@@ -73,28 +75,43 @@ namespace RE
 		std::uint16_t                                        pad03A;                              // 03A
 		ActorHandle                                          cameraTarget;                        // 03C
 		BSTSmallArray<TESCameraState*, CameraStates::kTotal> tempReturnStates;                    // 040
-		BSTSmartPointer<TESCameraState>                      cameraStates[CameraStates::kTotal];  // 0B8
-		Unk120*                                              unk120;                              // 120 - ?
-		NiPointer<bhkRigidBody>                              rigidBody;                           // 128 - ?
-		RefHandle                                            objectFadeHandle;                    // 130 - ?
-		mutable BSSpinLock                                   lock;                                // 134
-		float                                                worldFOV;                            // 13C
-		float                                                firstPersonFOV;                      // 140
-		NiPoint3                                             pos;                                 // 144 - ?
-		float                                                idleTimer;                           // 150 - ?
-		float                                                yaw;                                 // 154 - ? - in radians
-		std::uint32_t                                        unk158;                              // 158 - ?
-		std::uint32_t                                        unk15C;                              // 15C - ?
-		bool                                                 allowAutoVanityMode;                 // 160
-		bool                                                 bowZoomedIn;                         // 161
-		bool                                                 isWeapSheathed;                      // 162 - ?
-		bool                                                 isProcessed;                         // 163 - ?
-		std::uint8_t                                         unk164;                              // 164
-		std::uint8_t                                         unk165;                              // 165
-		std::uint16_t                                        pad166;                              // 166
+		BSTSmartPointer<TESCameraState>                      cameraStates[CameraStates::kTotal];  // 0B8, VR 0C0
+#ifndef SKYRIMVR
+		Unk120* unk120;                                                                           // 120 - ?
+#endif
+		NiPointer<bhkRigidBody> rigidBody;                                                        // 128, VR 130 - ?
+		RefHandle               objectFadeHandle;                                                 // 130, VR 138 - ?
+		mutable BSSpinLock      lock;                                                             // 134, VR 13C
+#ifdef SKYRIMVR
+		char VRpad140[20];                                                                        // VR 140
+#endif
+		float         worldFOV;                                                                   // 13C, VR 158
+		float         firstPersonFOV;                                                             // 140, VR 15C
+		NiPoint3      pos;                                                                        // 144, VR 160 - ?
+		float         idleTimer;                                                                  // 150, VR 16C - ?
+		float         yaw;                                                                        // 154, VR 170 - ? - in radians
+		std::uint32_t unk158;                                                                     // 158, VR 174 - ?
+		std::uint32_t unk15C;                                                                     // 15C, VR 178 - ?
+		bool          allowAutoVanityMode;                                                        // 160, VR 17C
+		bool          bowZoomedIn;                                                                // 161, VR 17D
+		bool          isWeapSheathed;                                                             // 162, VR 17E - ?
+		bool          isProcessed;                                                                // 163, VR 17F - ?
+		std::uint8_t  unk164;                                                                     // 164, VR 180
+		std::uint8_t  unk165;                                                                     // 165, VR 181
+		std::uint16_t pad166;                                                                     // 166, VR 182
 
 	private:
 		bool QCameraEquals(CameraState a_cameraState) const;
 	};
+#ifndef SKYRIMVR
 	static_assert(sizeof(PlayerCamera) == 0x168);
+#else
+	static_assert(offsetof(PlayerCamera, cameraStates) == 0xc0);
+	static_assert(offsetof(PlayerCamera, rigidBody) == 0x130);
+	static_assert(offsetof(PlayerCamera, lock) == 0x13c);
+	static_assert(offsetof(PlayerCamera, pos) == 0x160);
+	static_assert(offsetof(PlayerCamera, yaw) == 0x170);
+	static_assert(offsetof(PlayerCamera, allowAutoVanityMode) == 0x17c);
+	static_assert(sizeof(PlayerCamera) == 0x188);
+#endif
 }
