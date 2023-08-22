1fab24cf7cdbfc7dd97f456f9ae464531b100f8c
Alan Tse <alandtse@gmail.com>
2023-08-22 12:40:31 -0700

    fix: fix runtime offsets for NiParticles

--- include/RE/N/NiParticles.h
+++ include/RE/N/NiParticles.h
@@ -12,6 +12,15 @@ namespace RE
 		inline static constexpr auto RTTI = RTTI_NiParticles;
 		inline static constexpr auto Ni_RTTI = NiRTTI_NiParticles;
 
+		struct RUNTIME_DATA
+		{
+#define RUNTIME_DATA_CONTENT                           \
+	NiPointer<NiParticlesData> particleData; /*  158*/ \
+	std::uint64_t              unk160;       /*  160*/
+
+			RUNTIME_DATA_CONTENT
+		};
+		static_assert(sizeof(RUNTIME_DATA) == 0x10);
 		~NiParticles() override;  // 00
 
 		// override (BSGeometry)
@@ -24,8 +33,27 @@ namespace RE
 		void          SaveBinary(NiStream& a_stream) override;            // 1B
 		bool          IsEqual(NiObject* a_object) override;               // 1C - { return BSGeometry::IsEqual(); }
 
+		[[nodiscard]] inline RUNTIME_DATA& GetRuntimeData() noexcept
+		{
+			return REL::RelocateMember<RUNTIME_DATA>(this, 0x158, 0x198);
+		}
+
+		[[nodiscard]] inline const RUNTIME_DATA& GetRuntimeData() const noexcept
+		{
+			return REL::RelocateMember<RUNTIME_DATA>(this, 0x158, 0x198);
+		}
+
 		// members
-		NiPointer<NiParticlesData> particleData;  // 158
-		std::uint64_t              unk160;        // 160
+#ifndef SKYRIM_CROSS_VR
+		RUNTIME_DATA_CONTENT
+#endif
 	};
+#ifndef ENABLE_SKYRIM_VR
+	static_assert(sizeof(NiParticles) == 0x168);
+#elif !defined(ENABLE_SKYRIM_AE) && !defined(ENABLE_SKYRIM_SE)
+	static_assert(sizeof(NiParticles) == 0x1B0);
+#else
+	static_assert(sizeof(NiParticles) == 0x110);
+#endif
 }
+#undef RUNTIME_DATA_CONTENT
