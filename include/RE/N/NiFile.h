c96f770fc920864696e7d1524ce5c43c3b829fd0
Alan Tse <alandtse@gmail.com>
2023-12-13 00:30:54 -0800

    feat: add total member for NiFile

--- include/RE/N/NiFile.h
+++ include/RE/N/NiFile.h
@@ -32,7 +32,7 @@ namespace RE
 		std::uint32_t                             bufferAllocSize;  // 20
 		std::uint32_t                             bufferReadSize;   // 24
 		std::uint32_t                             pos;              // 28
-		std::uint32_t                             pad2C;            // 2C
+		std::uint32_t                             total;            // 2C
 		char*                                     buffer;           // 30
 		std::FILE*                                file;             // 38
 		stl::enumeration<OpenMode, std::uint32_t> mode;             // 40
