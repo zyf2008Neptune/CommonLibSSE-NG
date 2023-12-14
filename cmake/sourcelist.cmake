27db265efc034365dc82ef03c16bee76253cd975
RafearTheModder <RafearTheModder@gmail.com>
2023-12-14 03:24:52 -0600

    Add missed sourcelist.cmake line from prior RE

--- cmake/sourcelist.cmake
+++ cmake/sourcelist.cmake
@@ -95,6 +95,7 @@ set(SOURCES
 	include/RE/B/BGSEntryPointFunctionData.h
 	include/RE/B/BGSEntryPointFunctionDataActivateChoice.h
 	include/RE/B/BGSEntryPointFunctionDataOneValue.h
+	include/RE/B/BGSEntryPointFunctionDataSpellItem.h
 	include/RE/B/BGSEntryPointFunctionDataText.h
 	include/RE/B/BGSEntryPointPerkEntry.h
 	include/RE/B/BGSEquipSlot.h
