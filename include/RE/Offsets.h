0d835fd78cc268068d665eb90548183480c88bc8
Drew Warwick <dwarwick96@gmail.com>
2024-01-12 02:56:59 -0800

    Add BSSoundHandle::Pause

--- include/RE/Offsets.h
+++ include/RE/Offsets.h
@@ -146,6 +146,7 @@ namespace RE
 			inline constexpr REL::ID Play(static_cast<std::uint64_t>(67616));
 			inline constexpr REL::ID SetObjectToFollow(static_cast<std::uint64_t>(67636));
 			inline constexpr REL::ID SetPosition(static_cast<std::uint64_t>(67631));
+			inline constexpr REL::ID Pause(static_cast<std::uint64_t>(67618));
 			inline constexpr REL::ID Stop(static_cast<std::uint64_t>(67619));
 		}
 
