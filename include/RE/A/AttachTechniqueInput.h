800b4e071bffd0a6527387f0d9164e35b5ae4120
powerof3 <32599957+powerof3@users.noreply.github.com>
2024-01-27 00:02:44 +0530

    `ReferenceEffect` RE

--- include/RE/A/AttachTechniqueInput.h
+++ include/RE/A/AttachTechniqueInput.h
@@ -1,6 +1,5 @@
 #pragma once
 
-#include "RE/M/MagicSystem.h"
 #include "RE/N/NiSmartPointer.h"
 
 namespace RE
@@ -17,13 +16,13 @@ namespace RE
 			virtual ~AttachTechniqueInput();  // 00
 
 			// add
-			virtual void Unk_01(void);  // 01
+			virtual void Clear();  // 01
 
 			// members
-			NiPointer<NiNode>          current3DRoot;  // 08 - smart ptr
-			NiPointer<NiNode>          attachedArt;    // 10 - smart ptr
-			MagicSystem::CastingSource castingSource;  // 18
-			std::uint32_t              unk1C;          // 1C
+			NiPointer<NiNode> current3DRoot;  // 08 - smart ptr
+			NiPointer<NiNode> attachedArt;    // 10 - smart ptr
+			std::uint32_t     attachPoint;    // 18 - MagicSystem::CastingSource for casting art
+			std::uint32_t     pad18;          // 18
 		};
 		static_assert(sizeof(AttachTechniqueInput) == 0x20);
 	}
