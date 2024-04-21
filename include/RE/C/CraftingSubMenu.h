3aa81fe0aef546719cfff312f3993025409aaf40
powerof3 <32599957+powerof3@users.noreply.github.com>
2024-04-22 04:20:52 +0530

    Fix compile error

--- include/RE/C/CraftingSubMenu.h
+++ include/RE/C/CraftingSubMenu.h
@@ -34,7 +34,7 @@ namespace RE
 
 			void UpdateCraftingInfo(ActorValue a_actorValue)
 			{
-				using func_t = decltype(&UpdateCraftingInfo);
+				using func_t = decltype(&CraftingSubMenu::UpdateCraftingInfo);
 				REL::Relocation<func_t> func{ RELOCATION_ID(50461, 51364) };
 				return func(this, a_actorValue);
 			}
