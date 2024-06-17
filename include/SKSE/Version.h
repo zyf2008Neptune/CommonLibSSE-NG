14b749ee613e0d66b48e4844329bf65dc8fb886f
Neptune <zyfrtas2008@gmail.com>
2024-06-17 20:09:39 +0800

    fix version

--- include/SKSE/Version.h
+++ include/SKSE/Version.h
@@ -27,11 +27,11 @@ namespace SKSE
 	constexpr REL::Version RUNTIME_SSE_1_6_342(1, 6, 342, 0);
 	constexpr REL::Version RUNTIME_SSE_1_6_353(1, 6, 353, 0);
 	constexpr REL::Version RUNTIME_SSE_1_6_629(1, 6, 629, 0);
-    constexpr REL::Version RUNTIME_SSE_1_6_640(1, 6, 640, 0);
+	constexpr REL::Version RUNTIME_SSE_1_6_640(1, 6, 640, 0);
 	constexpr REL::Version RUNTIME_SSE_1_6_659(1, 6, 659, 0);
 	constexpr REL::Version RUNTIME_SSE_1_6_678(1, 6, 678, 0);
-	constexpr REL::Version RUNTIME_SSE_1_6_1330(1, 6, 1330, 0);
-	constexpr auto         RUNTIME_SSE_LATEST_AE = RUNTIME_SSE_1_6_1330;
+	constexpr REL::Version RUNTIME_SSE_1_6_1130(1, 6, 1130, 0);
+	constexpr auto         RUNTIME_SSE_LATEST_AE = RUNTIME_SSE_1_6_1130;
 	constexpr auto         RUNTIME_SSE_LATEST_SE = RUNTIME_SSE_1_5_97;
 	constexpr auto         RUNTIME_SSE_LATEST = RUNTIME_SSE_LATEST_AE;
 
